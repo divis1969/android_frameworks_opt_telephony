x9920/android_frameworks_opt_telephony_mtk

fix setInitialAttachApn request format for MTK
Change-Id: I574963ad2e026e517883badd99d469ad3e0f153f

src/java/com/android/internal/telephony/dataconnection/DcTrackerBase.java
@@ -1806,7 +1806,7 @@ public abstract class DcTrackerBase extends Handler {
         ApnSetting firstApnSetting = null;
         String operator = (r != null) ? r.getOperatorNumeric(): "";
 
-        log("setInitialApn: E mPreferredApn=" + mPreferredApn);
+        log("setInitialApn: E operator=" + operator + " mPreferredApn=" + mPreferredApn);
 
         if (apnList != null && !apnList.isEmpty()) {
             firstApnSetting = apnList.get(0);
@@ -1858,7 +1858,9 @@ public abstract class DcTrackerBase extends Handler {
 
             mPhone.mCi.setInitialAttachApn(initialAttachApnSetting.apn,
                     initialAttachApnSetting.protocol, initialAttachApnSetting.authType,
-                    initialAttachApnSetting.user, initialAttachApnSetting.password, null);
+                    initialAttachApnSetting.user, initialAttachApnSetting.password,
+                    operator, initialAttachApnSetting.canHandleType(PhoneConstants.APN_TYPE_IMS),
+                    null);
         }
     }
 

divis1969/android_frameworks_opt_telephony

src/java/com/android/internal/telephony/dataconnection/DcTrackerBase.java
@@ -1792,8 +1792,9 @@ public abstract class DcTrackerBase extends Handler {
         ApnSetting iaApnSetting = null;
         ApnSetting defaultApnSetting = null;
         ApnSetting firstApnSetting = null;
+        String operator = (r != null) ? r.getOperatorNumeric(): "";
 
-        log("setInitialApn: E preferredApn=" + preferredApn);
+        log("setInitialApn: E operator=" + operator + " preferredApn=" + preferredApn);
 
         if (apnList != null && !apnList.isEmpty()) {
             firstApnSetting = apnList.get(0);
@@ -1845,7 +1846,9 @@ public abstract class DcTrackerBase extends Handler {
 
             mPhone.mCi.setInitialAttachApn(initialAttachApnSetting.apn,
                     initialAttachApnSetting.protocol, initialAttachApnSetting.authType,
-                    initialAttachApnSetting.user, initialAttachApnSetting.password, null);
+                    initialAttachApnSetting.user, initialAttachApnSetting.password,
+                    operator, initialAttachApnSetting.canHandleType(PhoneConstants.APN_TYPE_IMS),
+                    null);
         }
     }
 